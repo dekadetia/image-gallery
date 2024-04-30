ddf670ae984d87ca78de4bd9a52d015b682caa1a
CodeFrenzyOfficial <teamcodefrenzy@gmail.com>
2024-04-30 15:55:20 +0500

    changes

--- components/navigationBar.jsx
+++ components/navigationBar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NavigationBar() {
     return (
         <div className="w-full flex justify-center items-center py-9">
-            <div className="w-full grid place-items-center space-y-4">
+            <div className="w-full grid place-items-center space-y-6">
                 
                 <Link href={"/"} >
                     <img src="/assets/logo.svg" className="object-contain w-40" alt="" />
