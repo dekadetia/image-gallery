339b568acc83274c732dbebf6977fb25edb96d27
Mohammad Waqas <waqasbaber581@gmail.com>
2024-05-08 16:37:51 +0500

    --load more

--- components/MasonaryGrid.jsx
+++ components/MasonaryGrid.jsx
@@ -1,12 +1,10 @@
-'use client';
-
-import { useState, useEffect } from "react";
+'use client'
 
 import "yet-another-react-lightbox/styles.css";
+import React, { useState, useEffect } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import Captions from "yet-another-react-lightbox/plugins/captions";
 import Loader from '../components/loader/loader';
-import { MdDelete } from "react-icons/md";
 
 export default function MasonaryGrid() {
     const [index, setIndex] = useState(-1);
@@ -14,13 +12,18 @@ export default function MasonaryGrid() {
     const [descriptionTextAlign, setDescriptionTextAlign] = useState("end");
     const [isOpen, setOpen] = useState(true);
     const [fetchPhotos, setFetchedPhotos] = useState([]);
-    const [slides, setSlides] = useState();
+    const [slides, setSlides] = useState([]);
     const [loader, setLoader] = useState(false);
+    const [skeleton, setSkeleton] = useState(false);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [newImages, setNewImages] = useState([]);
+
+    const arr = Array.from({ length: 35 }, (_, index) => index + 1);
 
     const getImages = async () => {
-        setLoader(true);
+        setSkeleton(true);
         try {
-            const response = await fetch("/api/firebase", {
+            const response = await fetch(`/api/firebase?page=${pageNumber}`, {
                 method: "GET",
             });
 
@@ -28,55 +31,105 @@ export default function MasonaryGrid() {
                 const data = await response.json();
                 const images = data.images;
                 console.log("Files fetched successfully:", images);
-                setFetchedPhotos(images);
-                setSlides(images.map((photo) => {
-                    const width = 1080 * 4;
-                    const height = 1620 * 4;
-                    return {
-                        src: photo.src,
-                        width,
-                        height,
-                        description: '',
-                    };
-                }));
-                setLoader(false);
+                setFetchedPhotos([...images]);
+                if (images.length > 35) {
+                    const slice = images.slice(0, 35);
+                    setNewImages(slice);
+                    setSlides(slice.map((photo) => {
+                        const width = 1080 * 4;
+                        const height = 1620 * 4;
+                        return {
+                            src: photo.src,
+                            width,
+                            height,
+                            description: '',
+                        };
+                    }));
+                } else {
+                    setNewImages(images);
+                    setSlides(images.map((photo) => {
+                        const width = 1080 * 4;
+                        const height = 1620 * 4;
+                        return {
+                            src: photo.src,
+                            width,
+                            height,
+                            description: '',
+                        };
+                    }));
+                }
+                setSkeleton(false);
             } else {
                 console.error("Failed to get files");
-                setLoader(false);
+                setSkeleton(false);
             }
         } catch (error) {
             console.error("Error fetching files:", error);
-            setLoader(false);
+            setSkeleton(false);
         }
+    };
+
+    const handleScroll = () => {
+        setSkeleton(true);
+        const nextImages = [...newImages, ...fetchPhotos.slice(newImages.length, newImages.length + 35)];
+        setNewImages(nextImages)
+        setSlides(nextImages.map((photo) => {
+            const width = 1080 * 4;
+            const height = 1620 * 4;
+            return {
+                src: photo.src,
+                width,
+                height,
+                description: '',
+            };
+        }));
+        setTimeout(() => {
+            setSkeleton(false);
+        }, 1500);
+
     };
 
     useEffect(() => {
         getImages();
-    }, [])
+    }, []);
 
     return (
         <>
-            {
-                loader && <div className="h-full flex items-center justify-center fixed w-full top-0 left-0 bg-black/50 z-10">
+            {/* {loader && (
+                <div className="h-full flex items-center justify-center fixed w-full top-0 left-0 bg-black/50 z-10">
                     <Loader />
                 </div>
-            }
+            )} */}
 
             <div className="c-container">
-                {fetchPhotos && fetchPhotos.length > 0 ? fetchPhotos.map((photo, i) => (
+                {newImages.map((photo, i) => (
                     <figure className="relative" key={i}>
                         <img
-                            key={i}
                             src={photo.src}
                             alt={'images'}
                             className="cursor-zoom-in images"
                             onClick={() => setIndex(i)}
+                            loading="lazy"
                         />
                     </figure>
-                )) :
-                    <div className="h-[60vh] flex items-center justify-center" />
+                ))}
+            </div>
+
+            <div className="c-container">
+                {skeleton &&
+                    arr.map((val, index) => {
+                        const heights = ['h-40', 'h-96', 'h-48', 'h-72', 'h-60', 'h-80'];
+                        const randomHeight = heights[Math.floor(Math.random() * heights.length)];
+                        return <div key={index} className={`bg-gray-700 w-full mb-2 animate-pulse shadow-lg ${randomHeight}`}></div>
+                    })
                 }
             </div>
+
+
+            <button className="bg-gray-600 h-12 font-medium text-base w-[200px] block mx-auto my-6" onClick={handleScroll}>
+                Load More
+            </button>
+
 
             {slides &&
                 <Lightbox
@@ -88,9 +141,7 @@ export default function MasonaryGrid() {
                     captions={{ isOpen, descriptionTextAlign, descriptionMaxLines }}
                 />
             }
-            <div className="md:text-sm lg:text-2xl">
-
-            </div>
+            <div className="md:text-sm lg:text-2xl"></div>
         </>
     )
 }
