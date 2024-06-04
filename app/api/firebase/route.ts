094367549504508457997675948472667bb8edd1
Mohammad Waqas <waqasbaber581@gmail.com>
2024-06-04 15:24:19 +0500

    --fields and limit

--- app/api/firebase/route.ts
+++ app/api/firebase/route.ts
@@ -1,4 +1,4 @@
-import { ref, getDownloadURL, uploadBytesResumable, listAll, deleteObject, getMetadata } from 'firebase/storage';
+import { ref, getDownloadURL, uploadBytesResumable, listAll, deleteObject, getMetadata, getStorage, list, updateMetadata } from 'firebase/storage';
 import { storage } from '../../../firebase/firebase-config';
 import { NextResponse } from "next/server";
 
@@ -7,18 +7,23 @@ export async function GET(request) {
   const listRef = ref(storage, 'images');
 
   try {
-    const res = await listAll(listRef);
+    const res = await list(listRef, { maxResults: 30 });
     const imagesWithData: any = await Promise.all(
       res.items.map(async (itemRef) => {
         const downloadURL = await getDownloadURL(itemRef);
-        const metadata:any = await getMetadata(itemRef);
+        const metadata: any = await getMetadata(itemRef);
         return {
           src: downloadURL,
           name: itemRef.name,
           created_at: metadata.timeCreated,
           updated_at: metadata.updated,
           size: metadata.size,
-          caption: metadata.customMetadata?.caption,
+          caption: metadata.customMetadata.caption ? metadata.customMetadata.caption : '',
+          director:  metadata.customMetadata.director ? metadata.customMetadata.director : '',
+          photographer:  metadata.customMetadata.photographer ? metadata.customMetadata.photographer : '',
+          year:  metadata.customMetadata.year ? metadata.customMetadata.year : '',
+          alphaname :  metadata.customMetadata.alphaname ? metadata.customMetadata.alphaname : '',
+          contentType: metadata.contentType,
         };
       })
     );
@@ -39,9 +44,13 @@ export async function GET(request) {
 export async function POST(request, response) {
   const formData = await request.formData();
   const caption = formData.get('caption');
+  const director = formData.get('director');
+  const photographer = formData.get('photographer');
+  const year = formData.get('year');
+  const alphaname = formData.get('alphaname');
 
   formData.forEach(async (data: any, index: any) => {
-    if(index != 'caption'){
+    if (index != 'caption') {
       const file = data;
       if (!file) {
         return NextResponse.json({ error: "No files received." }, { status: 400 });
@@ -49,12 +58,42 @@ export async function POST(request, response) {
       const metadata = {
         customMetadata: {
           caption: caption,
+          director: director,
+          photographer: photographer,
+          year: year,
+          alphaname: alphaname,
         },
       };
       const storageRef = ref(storage, `images/${file.name}`);
       await uploadBytesResumable(storageRef, file, metadata);
     }
   });
+
+  return NextResponse.json({ message: 'File uploaded successfully' }, { status: 200 });
+}
+
+
+// To handle a POST request to /api
+export async function PUT(request, response) {
+  const formData = await request.formData();
+  const filename = formData.get('file');
+  const caption = formData.get('caption');
+  const director = formData.get('director');
+  const photographer = formData.get('photographer');
+  const year = formData.get('year');
+  const alphaname = formData.get('alphaname');
+
+  const metadata = {
+    customMetadata: {
+      caption: caption,
+      director: director,
+      photographer: photographer,
+      year: year,
+      alphaname: alphaname,
+    },
+  };
+  const storageRef = ref(storage, `images/${filename}`);
+  await updateMetadata(storageRef, metadata);
 
   return NextResponse.json({ message: 'File uploaded successfully' }, { status: 200 });
 }
--- app/upload/page.jsx
+++ app/upload/page.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 import Loader from '../../components/loader/loader';
 import { ToastContainer, toast, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { MdDelete } from "react-icons/md";
+import { MdDelete, MdEdit } from "react-icons/md";
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from "yup"
@@ -19,8 +19,14 @@ export default function Page() {
     const [userIsLogged, setUserLogging] = useState(false)
     const [images, setImages] = useState([]);
     const [caption, setCaption] = useState('');
+    const [director, setDirector] = useState('');
+    const [photographer, setPhotographer] = useState('');
+    const [year, setYear] = useState('');
+    const [alphaname, setAlphaname] = useState('');
+
     const [loader, setLoader] = useState(false);
     const [modal, setModal] = useState(false);
+    const [editmodal, setEditModal] = useState(false);
     const [delId, setdelId] = useState();
     const [fetchPhotos, setFetchedPhotos] = useState([]);
     const { register, handleSubmit, formState: { errors } } = useForm({
@@ -42,6 +48,10 @@ export default function Page() {
         });
 
         formData.append('caption', caption)
+        formData.append('director', director)
+        formData.append('photographer', photographer)
+        formData.append('year', year)
+        formData.append('alphaname', alphaname)
 
         try {
             const response = await fetch("/api/firebase", {
@@ -109,6 +119,7 @@ export default function Page() {
             if (response.ok) {
                 const data = await response.json();
                 const images = data.images;
+                console.log(data.images)
                 setFetchedPhotos(images);
                 setLoader(false);
             } else {
@@ -163,6 +174,12 @@ export default function Page() {
         console.log(name, delId);
     }
 
+    const openEditModal = (name) => {
+        setEditModal(true);
+        setdelId(name);
+        console.log(name, delId);
+    }
+
     function isObjectMatch(obj1, obj2) {
         var obj1Keys = Object.keys(obj1);
         var obj2Keys = Object.keys(obj2);
@@ -196,6 +213,72 @@ export default function Page() {
         } else {
             setUserError("Credentials Are Wrong!");
         }
+    }
+
+    const updateImageData = async () => {
+        setLoader(true);
+        const formData = new FormData();
+
+        formData.append(`file`, delId);
+        formData.append('caption', caption)
+        formData.append('director', director)
+        formData.append('photographer', photographer)
+        formData.append('year', year)
+        formData.append('alphaname', alphaname)
+
+        try {
+            const response = await fetch("/api/firebase", {
+                method: "PUT",
+                body: formData,
+            });
+
+            if (response.ok) {
+                console.log("Files updated successfully");
+                // getImages();
+                toast.success("Files updated successfully!", {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    transition: Slide,
+                })
+                setLoader(false);
+            } else {
+                console.error("Failed to update file");
+                toast.error("Failed to update file!", {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    transition: Slide,
+                })
+                setLoader(false);
+            }
+        } catch (error) {
+            console.error("Error update file", error);
+            toast.error("Failed to update file!", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                transition: Slide,
+            })
+            setLoader(false);
+        }
+
+        setLoader(false);
     }
 
     useEffect(() => {
@@ -261,6 +344,18 @@ export default function Page() {
                     <label className="mb-2 mt-4">Add Caption</label>
                     <input type="text" onChange={(e) => setCaption(e.target.value)} name="caption" className="block w-full text-sm border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 h-10 p-4" />
 
+                    <label className="mb-2 mt-4">Add Director</label>
+                    <input type="text" onChange={(e) => setDirector(e.target.value)} name="director" className="block w-full text-sm border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 h-10 p-4" />
+
+                    <label className="mb-2 mt-4">Add Photographer</label>
+                    <input type="text" onChange={(e) => setPhotographer(e.target.value)} name="photographer" className="block w-full text-sm border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 h-10 p-4" />
+
+                    <label className="mb-2 mt-4">Add Year</label>
+                    <input type="text" onChange={(e) => setYear(e.target.value)} name="year" className="block w-full text-sm border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 h-10 p-4" />
+
+                    <label className="mb-2 mt-4">Add Alphaname</label>
+                    <input type="text" onChange={(e) => setAlphaname(e.target.value)} name="alphaname" className="block w-full text-sm border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 h-10 p-4" />
+
                     <label className="mb-2 mt-4">Add Files</label>
                     <input multiple onChange={changeHandler} className="block w-full text-sm border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 mb-6" id="file_input" type="file" />
                     <button disabled={images.length === 0} type="submit" className="bg-white text-black transition-all duration-300 hover:opacity-90 py-2 disabled:bg-gray-200 disabled:cursor-not-allowed">
@@ -292,6 +387,36 @@ export default function Page() {
                                 </span>
                             </p>
                             <p className="text-white font-bold my-2 truncate">
+                                Director :
+                                <span className="font-normal">
+                                    {photo.director}
+                                </span>
+                            </p>
+                            <p className="text-white font-bold my-2 truncate">
+                                Photographer :
+                                <span className="font-normal">
+                                    {photo.photographer}
+                                </span>
+                            </p>
+                            <p className="text-white font-bold my-2 truncate">
+                                Year :
+                                <span className="font-normal">
+                                    {photo.year}
+                                </span>
+                            </p>
+                            <p className="text-white font-bold my-2 truncate">
+                                Alphaname :
+                                <span className="font-normal">
+                                    {photo.alphaname}
+                                </span>
+                            </p>
+                            <p className="text-white font-bold my-2 truncate">
+                                Image Format :
+                                <span className="font-normal">
+                                    {photo.contentType}
+                                </span>
+                            </p>
+                            <p className="text-white font-bold my-2 truncate">
                                 Size :
                                 <span className="font-normal">
                                     {formatFileSize(photo.size)}
@@ -312,6 +437,10 @@ export default function Page() {
                             <button onClick={() => openModal(photo.name)}
                                 className="absolute top-5 right-5 rounded-full p-1 text-white bg-red-500 cursor-pointer">
                                 <MdDelete />
+                            </button>
+                            <button onClick={() => openEditModal(photo.name)}
+                                className="absolute top-5 right-14 rounded-full p-1 text-white bg-red-500 cursor-pointer">
+                                <MdEdit />
                             </button>
                         </div>
                     )) :
@@ -343,6 +472,49 @@ export default function Page() {
                                 <button onClick={() => setModal(false)} type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                                     No, cancel
                                 </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
+
+            {
+                editmodal == true &&
+                <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full h-screen  flex bg-black bg-opacity-50">
+                    <div className="relative p-4 w-full max-w-md max-h-full">
+                        <div className="relative bg-white rounded-lg shadow w-full">
+                            <button onClick={() => setEditModal(false)} type="button" className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                                <svg className="w-3 h-3" ariaHidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                </svg>
+                                <span className="sr-only">Close modal</span>
+                            </button>
+                            <div className="p-4 md:p-5 text-center">
+                                <div className="flex flex-col py-5 px-4 rounded-lg text-black">
+                                    <h2 className="w-full text-center text-2xl font-bold">Upload Images</h2>
+
+                                    <label className="mb-2 mt-4">Add Caption</label>
+                                    <input type="text" onChange={(e) => setCaption(e.target.value)} name="caption" className="block w-full text-sm border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 h-10 p-4" />
+
+                                    <label className="mb-2 mt-4">Add Director</label>
+                                    <input type="text" onChange={(e) => setDirector(e.target.value)} name="director" className="block w-full text-sm border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 h-10 p-4" />
+
+                                    <label className="mb-2 mt-4">Add Photographer</label>
+                                    <input type="text" onChange={(e) => setPhotographer(e.target.value)} name="photographer" className="block w-full text-sm border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 h-10 p-4" />
+
+                                    <label className="mb-2 mt-4">Add Year</label>
+                                    <input type="text" onChange={(e) => setYear(e.target.value)} name="year" className="block w-full text-sm border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 h-10 p-4" />
+
+                                    <label className="mb-2 mt-4">Add Alphaname</label>
+                                    <input type="text" onChange={(e) => setAlphaname(e.target.value)} name="alphaname" className="block w-full text-sm border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 h-10 p-4" />
+
+                                    <button type="button" onClick={() => updateImageData()} className="bg-white text-black my-4 transition-all duration-300 hover:opacity-90 py-2 bg-gray-200">
+                                            Update
+                                        </button>
+                                    <button onClick={() => setEditModal(false)} type="button" className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                                        Cancel
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
