0dd38c8fbbaa4af80550544bb05bcc8808ebd758
dekadetia <114835449+dekadetia@users.noreply.github.com>
2025-07-15 00:54:32 -0400

    Update page.jsx

--- app/rndm/page.jsx
+++ app/rndm/page.jsx
@@ -148,7 +148,7 @@ export default function Random() {
   <Link href={'/fade'}>
     <img
       src="/assets/crossfade.svg"
-      className="w-[1.4rem] object-contain transition-all duration-200 hover:scale-105 align-middle mr-[3.5px]"
+      className="w-[1.4rem] object-contain transition-all duration-200 hover:scale-105 align-middle mr-[3.75px]"
       alt=""
     />
   </Link>
@@ -161,7 +161,7 @@ export default function Random() {
 
   <IoMdShuffle
     onClick={getRandmImages}
-    className="cursor-pointer transition-all duration-200 hover:scale-105 text-2xl align-middle ml-[3.5px]"
+    className="cursor-pointer transition-all duration-200 hover:scale-105 text-2xl align-middle ml-[3.75px]"
   />
 </div>
 
