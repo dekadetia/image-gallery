648a6701950052277c281ba0c7ce85b1c5298655
dekadetia <114835449+dekadetia@users.noreply.github.com>
2025-07-20 09:40:25 -0400

    Update page.jsx

--- app/sand/page.jsx
+++ app/sand/page.jsx
@@ -246,31 +246,33 @@ export default function Order() {
               close={() => setIndex(-1)}
               plugins={[Video]}
               render={{
-                slideFooter: ({ slide }) => (
-                  <div
-                    className={cn(
-                      "lg:!w-[96%] text-left text-sm space-y-1 lg:pt-[.5rem] lg:mb-[.75rem] pb-[1rem] text-white px-0 pt-0 lg:pl-0 lg:ml-[-35px] lg:pr-[3rem] yarl-slide-content",
-                      slide.type === 'video' && isMobile && 'relative top-auto bottom-unset'
-                    )}
-                  >
-                    {slide.title && (
-                      <div className="yarl__slide_title">{slide.title}</div>
-                    )}
-                    <div className={cn("!space-y-0", slide.director && "!mb-5")}>
-                      {slide.director && (
-                        <div className="yarl__slide_description !text-[#99AABB]">
-                          <span className="font-medium">{slide.director}</span>
-                        </div>
-                      )}
-                      {slide.description && (
-                        <div className="yarl__slide_description">{slide.description}</div>
-                      )}
-                    </div>
-                  </div>
-                )
-              }}
-            />
-          )}
+  slideFooter: ({ slide }) => (
+    <div
+      className={cn(
+        "lg:!w-[96%] text-left text-sm space-y-1 lg:pt-[.5rem] lg:mb-[.75rem] pb-[1rem] text-white px-0 pt-0 lg:pl-0 lg:ml-[-35px] lg:pr-[3rem] yarl-slide-content",
+        slide.type === 'video' &&
+          typeof window !== 'undefined' &&
+          window.innerWidth < 768 &&
+          'relative top-auto bottom-auto'
+      )}
+    >
+      {slide.title && (
+        <div className="yarl__slide_title">{slide.title}</div>
+      )}
+      <div className={cn("!space-y-0", slide.director && "!mb-5")}>
+        {slide.director && (
+          <div className="yarl__slide_description !text-[#99AABB]">
+            <span className="font-medium">{slide.director}</span>
+          </div>
+        )}
+        {slide.description && (
+          <div className="yarl__slide_description">{slide.description}</div>
+        )}
+      </div>
+    </div>
+  )
+}}
+
         </div>
       ) : (
         <Loader />
