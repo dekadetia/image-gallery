559c3d951019b90660051ee920ee39c26434dca7
dekadetia <114835449+dekadetia@users.noreply.github.com>
2025-07-29 17:09:53 -0400

    Update page.jsx

--- app/fade/page.jsx
+++ app/fade/page.jsx
@@ -12,7 +12,6 @@ import Footer from '../../components/Footer';
 import Lightbox from 'yet-another-react-lightbox';
 import Video from 'yet-another-react-lightbox/plugins/video';
 import AudioPlayer from '../../components/AudioPlayer';
-import BlackModeCaption from '../../components/BlackModeCaption';
 import AnimatedLogo from '../../components/AnimatedLogo';
 
 export default function FadeGallery() {
@@ -288,12 +287,7 @@ transition={{ duration: 2, ease: 'easeInOut' }}
                     }}
                 />
             )}
-{blackMode && (
-  <>
-    <AudioPlayer blackMode={blackMode} showControls={showControls} />
-    <BlackModeCaption slide={slides[index]} showControls={showControls} />
-  </>
-)}
+            {blackMode && <AudioPlayer blackMode={blackMode} showControls={showControls} />}
         </RootLayout>
     );
 }
