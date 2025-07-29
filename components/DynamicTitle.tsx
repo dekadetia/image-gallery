76707370f8a3814715afc069b8d332fb9deed850
dekadetia <114835449+dekadetia@users.noreply.github.com>
2025-07-28 23:15:40 -0400

    Update DynamicTitle.tsx

--- components/DynamicTitle.tsx
+++ components/DynamicTitle.tsx
@@ -4,11 +4,27 @@ import { useEffect } from 'react'
 
 export default function DynamicTitle() {
   useEffect(() => {
-    const titles = [
-      '𝐓 | 𝐍 | 𝐃 | 𝐑 | 𝐁 | 𝐓 | 𝐍 | 𝐒',
-      '𝐁 | 𝐋 | 𝐍 | 𝐃 | 𝐆 | 𝐋 | 𝐒 | 𝐒',
-    ]
-    document.title = titles[Math.floor(Math.random() * titles.length)]
+    const titles = {
+      base: '𝐓 | 𝐍 | 𝐃 | 𝐑 | 𝐁 | 𝐓 | 𝐍 | 𝐒',
+      alt:  '𝐁 | 𝐋 | 𝐍 | 𝐃 | 𝐆 | 𝐋 | 𝐒 | 𝐒',
+    }
+
+    const setTitleFromLogoState = () => {
+      const logoState = sessionStorage.getItem('logoState') === 'alt' ? 'alt' : 'base'
+      document.title = titles[logoState]
+    }
+
+    setTitleFromLogoState()
+
+    document.addEventListener('visibilitychange', () => {
+      if (document.visibilityState === 'visible') {
+        setTitleFromLogoState()
+      }
+    })
+
+    return () => {
+      document.removeEventListener('visibilitychange', setTitleFromLogoState)
+    }
   }, [])
 
   return null
