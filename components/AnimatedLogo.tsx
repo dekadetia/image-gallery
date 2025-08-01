a53ce21afbf9fee906cfe8e4327701e4e9749295
dekadetia <114835449+dekadetia@users.noreply.github.com>
2025-08-01 01:20:41 -0400

    Update AnimatedLogo.tsx

--- components/AnimatedLogo.tsx
+++ components/AnimatedLogo.tsx
@@ -50,7 +50,7 @@ const isInitialAlt = typeof window !== 'undefined' && sessionStorage.getItem('lo
     ]
 
     let longPressed = false
-let userHasToggled = false
+    let firstToggle = true
     let isTouchInteraction = false
     let longPressTimer
 let toggled = false
@@ -68,7 +68,7 @@ if (saved === 'alt') {
 }
     const showAlt = (onComplete?: () => void) => {
       let completed = 0
-const exitDelay = !userHasToggled && isTouchInteraction ? 0.2 : 0
+      const exitDelay = firstToggle && isTouchInteraction ? 0.2 : 0
 
       for (let i = 1; i <= 8; i++) {
         const base = document.getElementById(`letter_${i}`)
@@ -145,7 +145,6 @@ const exitDelay = !userHasToggled && isTouchInteraction ? 0.2 : 0
       } else {
         showAlt(() => sessionStorage.setItem('logoState', 'alt'))
       }
-        userHasToggled = true
       toggled = !toggled
     }
 
