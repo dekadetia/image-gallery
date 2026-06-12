fbd261a6e482b1fb52316d2bec4f78e001d63a22
dekadetia <114835449+dekadetia@users.noreply.github.com>
2026-06-12 00:20:05 -0400

    Update DynamicTitle.tsx

--- components/DynamicTitle.tsx
+++ components/DynamicTitle.tsx
@@ -5,8 +5,8 @@ import { useEffect } from 'react'
 export default function DynamicTitle() {
   useEffect(() => {
     const titles = [
-      'TNDRBTNS/BLNDGLSS',
-      'TNDRBTNS/BLNDGLSS',
+      'TNDR/BTNS/BLND/GLSS',
+      'TNDR/BTNS/BLND/GLSS',
     ]
     document.title = titles[Math.floor(Math.random() * titles.length)]
   }, [])
